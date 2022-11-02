1c88f3526db44f7a34b44ba87adb33d77659fdbc
Nalbert de Jesus <nalbert-edm@outlook.com>
2022-11-02 17:55:35 -0300

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dataBaseURL = process.env.CONNECTIONSTRING;
-const PORT = process.env.PORT || 3000;
+
 
 mongoose
   .connect(process.env.CONNECTIONSTRING)
@@ -53,7 +53,7 @@ app.use(csrfMiddleware);
 app.use(routes);
 
 app.on("pronto", () => {
-  app.listen(PORT, () => {
+  app.listen(process.env.PORT || 3000, () => {
     console.log(`Listening on 3000`);
   });
 });
