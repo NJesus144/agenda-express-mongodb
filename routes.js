f56c9bff4fa111a8ac2cc5c6b83c0e76e87e56d0
Nalbert de Jesus <nalbert-edm@outlook.com>
2022-11-14 12:22:50 -0300

    add images and info

--- routes.js
+++ routes.js
@@ -7,16 +7,16 @@ const contatoController = require("./src/controllers/contatoController");
 
 const { loginRequired } = require("./src/middlewares/middleware");
 
-// Rotas da home
+
 route.get("/", homeController.Index);
 
-// Rotas de login
+
 route.get("/login/index", loginController.index);
 route.post("/login/register", loginController.register);
 route.post("/login/login", loginController.login);
 route.get("/login/logout", loginController.logout);
 
-//rotas de contato
+
 route.get("/contato/index", loginRequired, contatoController.index);
 route.post("/contato/register", loginRequired, contatoController.register);
 route.get("/contato/index/:id", loginRequired, contatoController.editIndex);
